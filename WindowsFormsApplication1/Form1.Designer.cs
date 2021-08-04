054cd2eab7d5273d6b66ea03154befa36293991b
YuHsienChan <fish20000309@gmail.com>
2021-08-04 20:08:12 +0800

    first commit

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.NbOfMoves = new System.Windows.Forms.Label();
+            this.NewGame = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources._02;
+            this.panel1.Controls.Add(this.button9);
+            this.panel1.Controls.Add(this.button8);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(12, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(392, 392);
+            this.panel1.TabIndex = 0;
+            // 
+            // button9
+            // 
+            this.button9.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources._01;
+            this.button9.Font = new System.Drawing.Font("Broadway BT", 20F);
+            this.button9.Location = new System.Drawing.Point(264, 265);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(125, 125);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.swap);
+            // 
+            // button8
+            // 
+            this.button8.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources._01;
+            this.button8.Font = new System.Drawing.Font("Broadway BT", 20F);
+            this.button8.Location = new System.Drawing.Point(134, 265);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(125, 125);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.swap);
+            // 
+            // button7
+            // 
+            this.button7.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources._01;
+            this.button7.Font = new System.Drawing.Font("Broadway BT", 20F);
+            this.button7.Location = new System.Drawing.Point(3, 265);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(125, 125);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.swap);
+            // 
+            // button6
+            // 
+            this.button6.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources._01;
+            this.button6.Font = new System.Drawing.Font("Broadway BT", 20F);
+            this.button6.Location = new System.Drawing.Point(265, 134);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(125, 125);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.swap);
+            // 
+            // button5
+            // 
+            this.button5.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources._01;
+            this.button5.Font = new System.Drawing.Font("Broadway BT", 20F);
+            this.button5.Location = new System.Drawing.Point(134, 134);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(125, 125);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.swap);
+            // 
+            // button4
+            // 
+            this.button4.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources._01;
+            this.button4.Font = new System.Drawing.Font("Broadway BT", 20F);
+            this.button4.Location = new System.Drawing.Point(3, 134);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(125, 125);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.swap);
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources._01;
+            this.button3.Font = new System.Drawing.Font("Broadway BT", 20F);
+            this.button3.Location = new System.Drawing.Point(265, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(125, 125);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.swap);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources._01;
+            this.button2.Font = new System.Drawing.Font("Broadway BT", 20F);
+            this.button2.Location = new System.Drawing.Point(134, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 125);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.swap);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources._01;
+            this.button1.Font = new System.Drawing.Font("Broadway BT", 20F);
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 125);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.swap);
+            // 
+            // NbOfMoves
+            // 
+            this.NbOfMoves.AutoSize = true;
+            this.NbOfMoves.BackColor = System.Drawing.Color.PeachPuff;
+            this.NbOfMoves.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.NbOfMoves.Font = new System.Drawing.Font("Broadway BT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NbOfMoves.Location = new System.Drawing.Point(428, 16);
+            this.NbOfMoves.Name = "NbOfMoves";
+            this.NbOfMoves.Size = new System.Drawing.Size(175, 21);
+            this.NbOfMoves.TabIndex = 1;
+            this.NbOfMoves.Text = "Number of Moves:";
+            // 
+            // NewGame
+            // 
+            this.NewGame.BackColor = System.Drawing.Color.PeachPuff;
+            this.NewGame.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources._01;
+            this.NewGame.Font = new System.Drawing.Font("Broadway BT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NewGame.Location = new System.Drawing.Point(428, 100);
+            this.NewGame.Name = "NewGame";
+            this.NewGame.Size = new System.Drawing.Size(128, 41);
+            this.NewGame.TabIndex = 2;
+            this.NewGame.Text = "New Game";
+            this.NewGame.UseVisualStyleBackColor = false;
+            this.NewGame.Click += new System.EventHandler(this.NewGame_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.PeachPuff;
+            this.button10.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources._01;
+            this.button10.Font = new System.Drawing.Font("Broadway BT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.Location = new System.Drawing.Point(428, 193);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(128, 41);
+            this.button10.TabIndex = 3;
+            this.button10.Text = "Auto";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources._02;
+            this.ClientSize = new System.Drawing.Size(673, 417);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.NewGame);
+            this.Controls.Add(this.NbOfMoves);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "8 puzzle game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label NbOfMoves;
+        private System.Windows.Forms.Button NewGame;
+        private System.Windows.Forms.Button button10;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static WindowsFormsApplication1.Form1;
+using System.Threading;
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int movesnum = 0, labelIndex = 0;
+        List<int> labellist = new List<int>();
+
+        private void shufffle()
+        {
+            labellist.Clear();
+
+            Random r = new Random();
+            foreach (Button btn in this.panel1.Controls)
+            {
+                while (labellist.Contains(labelIndex))
+                    labelIndex = r.Next(9);
+
+                btn.Text = (labelIndex == 0) ? "0" : labelIndex + "";
+                btn.BackColor = (btn.Text == "0") ? Color.White : Color.FromKnownColor(KnownColor.ControlLight);
+                labellist.Add(labelIndex);
+            }
+            
+            movesnum = 0;
+            NbOfMoves.Text = "Number of Moves : " + movesnum;
+        }
+
+        private void swap(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            if (btn.Text == "0")
+                return;
+
+            Button whitebtn = null;
+            foreach (Button bt in this.panel1.Controls)
+            {
+                if (bt.Text == "0")
+                {
+                    whitebtn = bt;
+                    break;
+                }
+            }
+
+            if (btn.TabIndex == (whitebtn.TabIndex - 1) || btn.TabIndex == (whitebtn.TabIndex - 3) || btn.TabIndex == (whitebtn.TabIndex + 1) || btn.TabIndex == (whitebtn.TabIndex + 3))
+            {
+                whitebtn.BackColor = Color.FromKnownColor(KnownColor.ControlLight);
+                btn.BackColor = Color.White;
+                whitebtn.Text = btn.Text;
+                btn.Text = "0";
+                movesnum++;
+                NbOfMoves.Text = "Number of Moves : " + movesnum;
+            }
+            checkOrder();
+        }
+
+        private void checkOrder()
+        {
+            int index = 0;
+            foreach (Button btn in this.panel1.Controls)
+            {
+                if (btn.Text != "0" && Convert.ToInt16(btn.Text) != index)
+                {
+                    return;
+                }
+                index++;
+            }
+            MessageBox.Show("恭喜！完成！你移動第" + movesnum + "次而已耶！");
+        }
+
+        private void NewGame_Click(object sender, EventArgs e)
+        {
+            shufffle();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            shufffle();
+        }
+
+        public class Node
+        {
+            public byte[] status; // 當前狀態
+            public Node father; // 紀錄上個狀態，如果father = null 該點為根節點
+            public Node(byte[] status, Node father)
+            {
+                this.status = status;
+                this.father = father;
+            }
+
+            // 把陣列轉換成數字序列，比對時就不用兩將陣列元素一一檢查了
+            // 像是 stauts = {8, 6, 4, 0, 7, 2, 5, 1, 3}，轉後後即可得到864072513
+            public int ToSequence()
+            {
+                int result = 0;
+                for (int i = 0; i < status.Length; i++)
+                    result = result * 10 + status[i];
+                return result;
+            }
+        }
+
+        List<Node> GetNext(Node now)// 傳入當前版面，回傳0所有移動後的狀態
+        {
+            int index = Array.IndexOf<byte>(now.status, 0);
+            int col = index % 3;
+            int row = index / 3;
+
+            List<Node> nextPush = new List<Node>();
+            byte[] next;
+            byte temp;
+
+            if (row != 0) // Top
+            {
+                next = (byte[])now.status.Clone();
+                //swap(ref next[index], ref next[index - 3]);// 跟上面交換   
+
+                temp = next[index];
+                next[index] = next[index - 3];
+                next[index - 3] = temp;
+
+                nextPush.Add(new Node(next, now));// 加入這個新狀態
+            }
+
+            if (col != 2) // Right
+            {
+                next = (byte[])now.status.Clone();
+                //swap(ref next[index], ref next[index + 1]);// 跟右邊交換
+
+                temp = next[index];
+                next[index] = next[index + 1];
+                next[index + 1] = temp;
+
+                nextPush.Add(new Node(next, now));
+            }
+
+            if (row != 2) // Bottom
+            {
+                next = (byte[])now.status.Clone();
+                //swap(ref next[index], ref next[index + 3]);// 跟下面交換
+
+                temp = next[index];
+                next[index] = next[index + 3];
+                next[index + 3] = temp;
+
+                nextPush.Add(new Node(next, now));
+            }
+
+            if (col != 0) // Left
+            {
+                next = (byte[])now.status.Clone();
+               // swap(ref next[index], ref next[index - 1]);// 跟左邊交換
+
+                temp = next[index];
+                next[index] = next[index - 1];
+                next[index - 1] = temp;
+
+                nextPush.Add(new Node(next, now));
+            }
+
+            return nextPush;
+        }
+
+        // 傳入原本的版面和目標版面，回傳最短路徑
+        List<Node> Solve(byte[] source, byte[] goal)
+        {
+            Queue<Node> queue = new Queue<Node>();
+
+            // 使用狀態序列來儲存已走過的路徑，防止往回走
+            SortedList<int, bool> book = new SortedList<int, bool>();
+
+            Node end = new Node(goal, null);// 終點
+            Node start = new Node(source, null);// 起點
+
+            queue.Enqueue(start);// 推入起點
+            book.Add(start.ToSequence(), true);// 標示起點已走過，防止走回頭路
+
+            int endStatus = end.ToSequence();
+            while (queue.Count > 0)
+            {
+                // 取得當前搜索狀態，並移出佇列
+                Node now = queue.Dequeue();
+
+                // 如果抵達終點，那就輸出路徑
+                if (now.ToSequence() == endStatus)
+                    return PathTrace(now);
+
+                // 取得能走的位置
+                List<Node> nextPath = GetNext(now);
+                foreach (var path in nextPath)
+                {
+                    int sign = path.ToSequence();
+
+                    // 判斷當前節點狀態是否擴展過了
+                    if (!book.Keys.Contains(sign))
+                    {
+                        // 推入當前狀態，並標記該路徑已走過，因為每個狀態只需要擴展一次就夠了                
+                        queue.Enqueue(path);
+                        book.Add(sign, true);
+                    }
+                }
+            }
+
+            // 如果窮舉完都沒找到，代表無解
+            return null;
+        }
+        public String str = "";
+        public String[,] buttonstr = new string[1000,9];
+        public int n = 0;
+
+        List<Node> PathTrace(Node now)
+        {
+            // 回朔路徑
+            List<Node> path = new List<Node>();
+            
+
+            while (now.father != null)
+            {
+                path.Add(now);
+                now = now.father;
+
+                str += now.status[0].ToString() + now.status[1].ToString() + now.status[2].ToString() + "\n" + now.status[3].ToString() + now.status[4].ToString() + now.status[5].ToString() + "\n" + now.status[6].ToString() + now.status[7].ToString() + now.status[8].ToString() + "\n";
+                str += "---------------------------" + "\n";
+
+                for (int i =0;i<=8;i++)
+                {
+                    buttonstr[n, i] = now.status[i].ToString();
+                }
+
+                n += 1;
+            }
+            path.Reverse();
+            return path;
+        }
+
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            button1.Text = labellist[8].ToString();
+            button2.Text = labellist[7].ToString();
+            button3.Text = labellist[6].ToString();
+            button4.Text = labellist[5].ToString();
+            button5.Text = labellist[4].ToString();
+            button6.Text = labellist[3].ToString();
+            button7.Text = labellist[2].ToString();
+            button8.Text = labellist[1].ToString();
+            button9.Text = labellist[0].ToString();
+            //以上為按鈕相對應的值
+
+            byte[] source = new byte[] { byte.Parse(button1.Text), byte.Parse(button2.Text), byte.Parse(button3.Text), byte.Parse(button4.Text), byte.Parse(button5.Text), byte.Parse(button6.Text), byte.Parse(button7.Text), byte.Parse(button8.Text), byte.Parse(button9.Text) };
+            byte[] goal = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 0 }; //答案
+            
+            List<Node> path = Solve(source, goal);
+
+            //MessageBox.Show(buttonstr[0,0]);
+           
+            for (int i = 2; i <= n; i++)
+            {
+                MessageBox.Show("第"+(i-1).ToString()+"步");
+                button1.Text = buttonstr[n - i, 0];
+                button2.Text = buttonstr[n - i, 1];
+                button3.Text = buttonstr[n - i, 2];
+                button4.Text = buttonstr[n - i, 3];
+                button5.Text = buttonstr[n - i, 4];
+                button6.Text = buttonstr[n - i, 5];
+                button7.Text = buttonstr[n - i, 6];
+                button8.Text = buttonstr[n - i, 7];
+                button9.Text = buttonstr[n - i, 8];
+                Thread.Sleep(1000);
+                
+            }
+
+            StreamWriter strr = new StreamWriter(@"C:\Users\user\OneDrive\桌面\tree.txt"); //順序是倒過來的，所以步驟是由下而上
+            string WriteWord = str;
+            strr.WriteLine(WriteWord);
+            strr.Close();
+
+            //-----------------------------------------------------------------
+            byte UTF8bytes1 = 6;
+            string UTF8String1 = UTF8bytes1.ToString(); //位元轉字串OK
+            //-----------------------------------------------------------------
+
+            //-----------------------------------------------------------------
+            string UTF8String = "6"; 
+            byte UTF8bytes =  byte.Parse(UTF8String);  //字串轉位元OK
+            //-----------------------------------------------------------------
+            MessageBox.Show("最少步驟為:" + (n+1) + "步");
+        }
+    }  
+}
